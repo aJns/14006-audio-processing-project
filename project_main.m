1f0664ad386e3fdd6c6f4b6175f2205522a7c2a0
Jonas Nikula <jonas.nikula@student.tut.fi>
2016-10-12 23:42:55 +0300

    Fix faulty whitespace

--- project_main.m
+++ project_main.m
@@ -56,28 +56,28 @@ MASK_dB = 16;
 
 
 X_dft = zeros(frameSamples,floor(length(testSample)/frameSamples));
-% We now have the sub­band signals for M bands, given by the analysis
-% block
-si=1;ei=NDFT; % Start and End indices of input signal for each frame
+% We now have the sub band signals for M bands, given by the analysis
+% block
+si=1;ei=NDFT; % Start and End indices of input signal for each frame
 
 win=0;
-% Main loop
+% Main loop
 while(ei < length(x_input_signal))
-    win=win+1; % keep track of number of processed windows/frames
-    % indices of datapoints of full­band original signal
-    % for the psychoacoustic model:
+    win=win+1; % keep track of number of processed windows/frames
+    % indices of datapoints of full band original signal
+    % for the psychoacoustic model:
     time_index_fullband = si:ei;
     
     X_dft(:,win) = fft(x_input_signal(time_index_fullband));
     
-    % Subband time indices at 1/M sampling rate (for each band).
-    % these sample indices need to be then quantized in this window
+    % Subband time indices at 1/M sampling rate (for each band).
+    % these sample indices need to be then quantized in this window
     time_index_subband = max((si-1)/M,1):ei/M;
     
-    % The most important part: SPL, Masking threshold, SMR and
-    % quantization here for samples, and count the number of bits
-    % used, e.g. sum up how many sub­band samples were quantized with
-    % nbits over all sub­bands and all processing frames.
+    % The most important part: SPL, Masking threshold, SMR and
+    % quantization here for samples, and count the number of bits
+    % used, e.g. sum up how many sub band samples were quantized with
+    % nbits over all sub bands and all processing frames.
     
     % normalized SPL calculation
     freq1k = 1000;
@@ -134,11 +134,11 @@ while(ei < length(x_input_signal))
     end
     
     
-    si=si+NDFT; % advance 1 full frame in input data (no overlap)
+    si=si+NDFT; % advance 1 full frame in input data (no overlap)
     ei=si+NDFT-1;
 end
-% Proceed to reconstruct the signal from the quantized subband
-% signals as in exercise 4.
+% Proceed to reconstruct the signal from the quantized subband
+% signals as in exercise 4.
 
 
 
